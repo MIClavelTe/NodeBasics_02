7d1c5917a6c37488d889163a71f590d63a2d40bc
MIClavelTe <miclavelte@gmail.com>
2018-07-22 16:45:46 -0700

    Create GET request

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
+const username = "miclavelte";
+const https = require('http');
+
 function printMessage(username, badges, points) {
     const message = `${username} has ${badges} badge(s) and ${points} points`;
     console.log(message);
 }
 
-
+const request = https.get(`http://teamtreehouse.com/${username}.json`, response => {
+    console.log(response.statusCode);
+});
