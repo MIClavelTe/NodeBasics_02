b147beb672ac7216d706ffd142a8693e0663441f
MIClavelTe <miclavelte@gmail.com>
2018-07-22 17:30:49 -0700

    Get json from user

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const username = "miclavelte";
-const https = require('http');
+const https = require('https');
 
-function printMessage(username, badges, points) {
-    const message = `${username} has ${badges} badge(s) and ${points} points`;
+function printMessage(user, badges, points) {
+    const message = `${user} has ${badges} badge(s) and ${points} points`;
     console.log(message);
 }
 
-const request = https.get(`http://teamtreehouse.com/${username}.json`, response => {
-    console.log(response.statusCode);
+const request = https.get(`https://teamtreehouse.com/${username}.json`, response => {
+    let body = ""
+
+    response.on('data', data => {
+        body += data.toString();
+    });
+
+    response.on('end', () => {
+        console.log(body);
+    });
 });
